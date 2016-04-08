c32bdf3828d2b1313fb3327256258af3263f2bec
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-08 16:34:21 +0200

    Build system skeleton

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'guclink_auth_www'
+set :repo_url, 'git@github.com:ah450/guclink-auth-www.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/root/guclink_auth_www'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 1
+
+namespace :deploy do
+
+  desc 'Install node modules'
+  task :install_node_modules do
+    on roles(:web) do
+      within release_path do
+        execute :npm, 'install'
+      end
+    end
+  end
+
+  desc 'Deploy application'
+  task :deploy_npm do
+    on roles(:web) do
+      within release_path do
+        execute :npm, 'run', 'deploy'
+      end
+    end
+  end
+
+  desc 'Chown srv to nginx'
+  task :set_nginx_permissions do
+    on roles(:web) do
+      execute :chown, '-R', 'nginx:nginx', '/srv/www'
+      execute :restorecon, '-R', '-v', '/srv/www'
+    end
+  end
+
+  after :updated, :install_node_modules
+  after :updated, :deploy_npm
+  after :publishing, :set_nginx_permissions
+end
---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var gzip = require('gulp-gzip');
+var merge = require('merge2');
+var concat = require('gulp-concat');
+var minimist = require('minimist');
+var uglify = require("gulp-uglify");
+var ngAnnotate = require('gulp-ng-annotate');
+var minifyHtml = require('gulp-htmlmin');
+var connect = require('gulp-connect');
+var bower = require('gulp-bower');
+var mainBowerFiles = require('main-bower-files');
+var css = require('./gulp-tasks/css');
+var assets = require('./gulp-tasks/assets');
+var polyfills = require('./gulp-tasks/polyfills');
+var templates = require('./gulp-tasks/templates');
+var scripts = require('./gulp-tasks/scripts');
+var rimraf = require('rimraf');
+var watch = require('gulp-watch');
+
+gulp.task('bower-install', function() {
+  // Runs bower install
+  return bower()
+    .pipe(gulp.dest('./bower_components'));
+});
+
+gulp.task('bower', ['bower-install', 'create-dirs'], function() {
+  // moves main files to lib folder
+  return gulp.src(mainBowerFiles(), {base: 'bower_components'})
+    .pipe(gulp.dest('libs'));
+});
+
+
+gulp.task('bower-dev', ['bower-install', 'create-dirs'], function() {
+  // moves main files to lib folder
+  return gulp.src(mainBowerFiles({
+    includeDev: 'inclusive'
+  }), {base: 'bower_components'})
+    .pipe(gulp.dest('libs'));
+});
+
+gulp.task('create-dirs', function() {
+  var dirs = ['build', 'dist', 'libs', 'test', 'compiledSpecs'];
+  rimraf.sync('libs');
+  dirs.forEach(function(dir) {
+    try {
+      fs.mkdirSync(dir);
+    } catch(e) {
+      if (e.code != 'EEXIST') {
+        throw e;
+      }
+    }
+  });
+});
+
+
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['create-dirs', 'bower'], function() {
+    var cssStream = css.processSass();
+    var scriptStream = scripts.processScripts();
+    var dependenciesStream = scripts.processDeps();
+    var routesStream = scripts.processRoutes();
+    var templatesStream = templates.processTemplates();
+    var index = gulp.src('src/index.html');
+    var assetsStream = assets.processAssets();
+    var polyfillsStream = polyfills.processPolyfills();
+    var js = merge(dependenciesStream, templatesStream, scriptStream, routesStream)
+      .pipe(concat('app.js'));
+    var streams = merge([cssStream, js, assetsStream, polyfillsStream, index]);
+    return streams.pipe(gulp.dest('build'))
+});
+
+var options = minimist(process.argv.slice(2), {
+  string: 'dest',
+  default: {dest: 'dist'}
+})
+
+gulp.task('production-helper', ['create-dirs', 'bower'], function() {
+  var cssStream = css.minifyCss(css.processSass());
+  var scriptStream = scripts.processScripts().pipe(ngAnnotate()).pipe(uglify());
+  var dependenciesStream = scripts.processDeps().pipe(uglify());
+  var routesStream = scripts.processRoutes().pipe(uglify({mangle: false}));
+  var templatesStream = templates.minifyTemplates();
+  var assetsStream = assets.processAssets();
+  var index = gulp.src('src/index.html').pipe(minifyHtml({
+      empty: true
+    }));
+  var polyfillsStream = polyfills.processPolyfills();
+  var js = merge(dependenciesStream, templatesStream, scriptStream, routesStream)
+    .pipe(concat('app.js'));
+  var streams = merge([cssStream, js, assetsStream, polyfillsStream, index]);
+  return streams
+    .pipe(gulp.dest(options.dest));
+});
+
+gulp.task('production', ['production-helper'], function () {
+  var src = [options.dest, '**', '*.{html,css,js,eot,svg,ttf,otf,json}'].join('/');
+  return gulp.src(src)
+    .pipe(gzip({ gzipOptions: { level: 9 } }))
+    .pipe(gulp.dest(options.dest));
+});
+
+
+gulp.task('server', ['build'], function() {
+  var proxyOpts = {
+    target: 'http://localhost:3000',
+    ws: true
+  }
+  connect.server({
+    livereload: true,
+    root: 'build',
+    port: 8000,
+    https: true
+  });
+});
+
+gulp.task('reload', ['build'], function() {
+  return gulp.src('./build/**/*')
+    .pipe(connect.reload());
+});
+
+gulp.task('dev', ['watch', 'server']);
+
+gulp.task('watch', function() {
+  watch(['src/**', 'images/**', 'assets/**', 'polyfills/**', 'fonts/**', 'bower.json'], function() {
+    gulp.start('reload');
+  });
+});
+
+
+
+gulp.task('build-test', ['create-dirs', 'bower-dev'], function() {
+  var scriptStream = scripts.processScripts();
+  var dependenciesStream = scripts.processDeps().pipe(concat('app-deps.js'));
+  var routesStream = scripts.processRoutes();
+  var templatesStream = templates.processTemplates();
+  var js = merge(templatesStream, scriptStream, routesStream)
+    .pipe(concat('app-testing.js'))
+  var appStreams = merge([dependenciesStream, js]).pipe(gulp.dest('test'))
+  var specStream = scripts.processSpecs().pipe(gulp.dest('compiledSpecs'))
+  return merge([appStreams, specStream]);
+});
+
+
+gulp.task('test', ['build-test'], function (done) {
+  var Server = require('karma').Server;
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
